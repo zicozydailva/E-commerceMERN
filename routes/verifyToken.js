931bd2fd332e5c372a8e6709f98e339eee01813a
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-04 02:07:42 +0100

    admin setup

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -21,3 +21,15 @@ const verifyTokenAndAuthorization = (req, res, next) => {
         res.status(403).json("You are not alowed to do that!");
       }
 }
+
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You are not allowed to do that")
+        }
+    })
+}
+
+module.exports = {verifyToken, verifyTokenAndAdmin, verifyTokenAndAuthorization}
